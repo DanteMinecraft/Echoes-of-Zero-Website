263b67baec021a8a89696b77837b103210475163
DanteMinecraft <dantewallen07@gmail.com>
2026-03-08 22:58:08 +0100

    fixed colors for nav and footer on blog post page. Also change description

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,13 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Echoes of Zero",
-  description: "Made by DanteMC",
+  description: "Follow the evolving story of Island Zero — explore the lore, maps, and updates behind the Echoes of Zero universe.",
+  openGraph: {
+    title: "Echoes of Zero",
+    description:
+      "Explore the story of Island Zero — maps, lore, and updates from the Echoes of Zero universe.",
+    images: ["../public/echoes_of_zero.png"],
+  }
 };
 
 export default function RootLayout({
